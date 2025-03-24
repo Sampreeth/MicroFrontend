836def716ef5f34dbc24d47fb22e60e6acc5fc1b
Sampreeth <sampreeth10@gmail.com>
2025-03-24 10:58:54 +0530

    fereration happening

---
+++ container/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Cart from "app1/Cart";
+
+function App() {
+
+  return (
+    <>
+     <Cart />
+    </>
+  )
+}
+
+export default App
---
+++ container/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "container",
+      filename: "remoteEntry.js",
+      remotes: {
+        app1: "http://localhost:4173/assets/remoteEntry.js",
+      },
+      shared: ["react"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server:{
+    port:3000
+  }
+})
